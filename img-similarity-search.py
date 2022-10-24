16e30510f29e9e2a3d419a6cc55c44eaf39bf845
Matthew <80968027+MLaunch@users.noreply.github.com>
2022-10-24 14:56:09 -0400

    Update img-similarity-search.py

--- img-similarity-search.py
+++ img-similarity-search.py
@@ -34,6 +34,11 @@ def lambda_handler(event, context):
 
 #Determine Probabilities
 def generateComparison(dsImgFeatures):
+    #loop over retrieved pickles
+    for row in dsImgFeatures:
+        vectors.append(DeVectorize(row["Pickle"]))
+        documents.append(row["AssetId"])
+
     neighbors = NearestNeighbors(n_neighbors=5, algorithm='brute', metric='euclidean').fit(feature_list)
     
 
@@ -49,3 +54,30 @@ def retrievePickles(sTableName):
     print("pickles list")
     print(data)
     return data
+
+def DeVectorize(sContent):
+    # convert document to vector of word embeddings
+    print("Decode")
+    #print(sContent)
+    #print(base64.b64decode(sContent))
+    #serialized_embedding = pickle.loads(base64.b64decode(sContent))
+    print(sContent)
+    print(type(sContent))
+    print(base64.b64decode(sContent))
+    print(type(base64.b64decode(sContent)))
+    
+    #serialized_embedding = pickle.loads(base64.b64decode(sContent))
+#    serialized_embedding = pickle.loads(sContent)
+
+    sContent = sContent[2:len(sContent)-1]
+    print(sContent)
+    sContent2 = bytes(sContent, 'utf-8')
+    print(sContent2)
+    sContent3 = base64.b64decode(sContent)
+    print("decoded")
+    print(sContent3)
+    print("pickle load")
+    serialized_embedding = pickle.loads(sContent3)
+    print(serialized_embedding)
+    
+    return serialized_embedding
